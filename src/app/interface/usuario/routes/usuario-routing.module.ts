6b3c4cfb5bab46117ccf604df45b434b5cbcbdc0
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-11-11 19:05:22 -0300

    ajustes nas rotas de usuário

--- src/app/interface/usuario/routes/usuario-routing.module.ts
+++ src/app/interface/usuario/routes/usuario-routing.module.ts
@@ -1,7 +1,24 @@
+import { UsuarioFormularioComponent } from './../view/usuario/usuario-formulario/usuario-formulario.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuarioPesquisarComponent } from '../view/usuario/usuario-pesquisar/usuario-pesquisar.component';
 
-const moduleRoutes: Routes = [];
+const moduleRoutes: Routes = [
+  {
+    path: 'usuario/pesquisar',
+    component: UsuarioPesquisarComponent
+  }
+  ,
+  {
+    path: 'usuario/incluir',
+    component: UsuarioFormularioComponent
+  }
+  ,
+  {
+    path: 'usuario/:codigo/alterar',
+    component: UsuarioFormularioComponent
+  }
+];
 
 @NgModule({
     exports: [RouterModule],
