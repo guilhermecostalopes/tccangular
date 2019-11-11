08f539dadea9de26482cb3acda6435ec3435c508
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-11-11 19:24:43 -0300

    inclusão dos componentes primeng no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { UsuarioModule } from './interface/usuario/usuario.module';
-import { GrupoModule } from './interface/grupo/grupo.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
+
+import { UsuarioModule } from './interface/usuario/usuario.module';
+import { GrupoModule } from './interface/grupo/grupo.module';
 
 import localeBr from '@angular/common/locales/pt';
 registerLocaleData(localeBr);
@@ -16,6 +18,7 @@ registerLocaleData(localeBr);
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     GrupoModule,
     UsuarioModule
