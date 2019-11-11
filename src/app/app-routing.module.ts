6093640188c231d1f4d0c80e1ee1818c41fa1d17
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-11-11 19:47:21 -0300

    ajustes compontes primeng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PrincipalComponent } from './interface/principal/principal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: PrincipalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,18 +10,33 @@ import { UsuarioModule } from './interface/usuario/usuario.module';
 import { GrupoModule } from './interface/grupo/grupo.module';
 
 import localeBr from '@angular/common/locales/pt';
+import { PrincipalComponent } from './interface/principal/principal.component';
+
+import { SidebarModule } from 'primeng/sidebar';
+import { FieldsetModule } from 'primeng/primeng';
+
 registerLocaleData(localeBr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     GrupoModule,
-    UsuarioModule
+    UsuarioModule,
+    SidebarModule,
+    FieldsetModule
+  ],
+  exports: [
+    SidebarModule,
+    FieldsetModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [
     {
