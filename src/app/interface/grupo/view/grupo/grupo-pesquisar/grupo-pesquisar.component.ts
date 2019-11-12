60e080aa5950a2d070936671dff797b941c486a2
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-11-11 21:11:12 -0300

    ajustes grupo pesquisar

--- src/app/interface/grupo/view/grupo/grupo-pesquisar/grupo-pesquisar.component.ts
+++ src/app/interface/grupo/view/grupo/grupo-pesquisar/grupo-pesquisar.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GHComponente } from 'src/app/ghComponente/ghcomponente';
+import { GrupoServiceService } from '../../../service/grupo-service.service';
+
+import { Router, ActivatedRoute } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Grupo } from '../../../model/grupo';
 
 @Component({
   selector: 'app-grupo-pesquisar',
   templateUrl: './grupo-pesquisar.component.html',
   styleUrls: ['./grupo-pesquisar.component.css']
 })
-export class GrupoPesquisarComponent implements OnInit {
+export class GrupoPesquisarComponent extends GHComponente {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    protected grupoService: GrupoServiceService,
+    protected router: Router,
+    protected routaAtual: ActivatedRoute,
+    protected confirmationService: ConfirmationService,
+    protected messageService: MessageService
+  ) {
+    super(
+      // tslint:disable-next-line:new-parens
+      new Grupo,
+      'Grupo',
+      'grupo',
+      grupoService,
+      router,
+      routaAtual,
+      confirmationService,
+      messageService
+    );
   }
-
 }
