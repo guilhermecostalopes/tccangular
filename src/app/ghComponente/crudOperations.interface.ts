4b1197462eb9071a2c952a2d30c32b5fac91a839
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-11-12 22:08:18 -0300

    ajustes em CrudOperations

--- src/app/ghComponente/crudOperations.interface.ts
+++ src/app/ghComponente/crudOperations.interface.ts
@@ -2,13 +2,8 @@ import 'rxjs/add/operator/map';
 
 export interface CrudOperations<T, ID> {
 salvar(t: T);
-bloquear(t: T);
-desbloquear(t: T);
-inativo(t: T);
+alterar(t: T);
 pesquisar(t: T);
 excluir(id: ID);
 buscarPorId(id: ID);
-buscarTodosStatusDoRegistro();
-buscarTodosSimNao();
-imprimirTodos();
 }
