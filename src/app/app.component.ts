5a611a5d6a044005f46814950373a5b0f11bbe36
Guilherme Lopes <guilherme@netsoft.eti.br>
2019-12-05 14:31:25 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  items: MenuItem[];
+
   title = 'tccguilherme';
+
+  ngOnInit() {
+    this.items = [
+        {
+          label: 'Grupos',
+          icon: 'pi pi-folder-open',
+          routerLink: ['/grupo/pesquisar']
+        },
+        {
+          separator: true
+        },
+        {
+          label: 'Usuários',
+          icon: 'pi pi-users',
+          routerLink: ['/usuario/pesquisar']
+        }
+      ];
+    }
 }
